401403c7c607997bcf3aa46dfeebe485b66ac4da
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-06-11 02:15:59 +0900

    Update express_server.js

--- src/express_server.js
+++ src/express_server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get("/",(req,res)=>{
-  res.send("hi");
+app.get("/1",(req,res)=>{
+  res.send("req1");
+});
+
+app.get("/2",(req,res)=>{
+  res.send("req2");
 });
 
 app.listen(3000, (err)=>{

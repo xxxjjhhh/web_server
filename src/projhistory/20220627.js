962072cbeb8a8c269395bc4e87f0d1b7c96c4e72
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-06-27 04:32:28 +0900

    Create 20220627.js

---
+++ src/projhistory/20220627.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+
+const privatekey = fs.readFileSync(__dirname + '/my/private.pem', 'utf8');
+const certkey = fs.readFileSync(__dirname + '/my/cert.pem', 'utf8');
+const csrkey = fs.readFileSync(__dirname + '/my/csr.pem', 'utf8');
+
+const sslkey = {
+        key: privatekey,
+        cert: certkey,
+        ca: csrkey
+};
+
+const app = express();
+const app443 = express();
+const httpsServer = https.createServer(sslkey, app443);
+
+
+app.get('/', (req, res) => {res.sendFile(__dirname + '/pages/index.html');});
+app443.get('/', (req, res) => {res.sendFile(__dirname + '/pages/index.html');});
+
+app.listen(80, () => {console.log('80 port server start');});
+httpsServer.listen(443, () => {console.log('443 port server start');});
+
+//ssl 파일은 미첨부

3fa63350b381fc430cff0a95343240d982a721ee
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-06-28 07:44:08 +0900

    Create 80443server_basic.js

---
+++ src/80443server_basic.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+
+const privatekey = fs.readFileSync(__dirname + '/my/private.pem', 'utf8');
+const certkey = fs.readFileSync(__dirname + '/my/cert.pem', 'utf8');
+const csrkey = fs.readFileSync(__dirname + '/my/csr.pem', 'utf8');
+
+const sslkey = {
+        key: privatekey,
+        cert: certkey,
+        ca: csrkey
+};
+
+const app = express();
+const app443 = express();
+const httpsServer = https.createServer(sslkey, app443);
+
+
+app.get('/', (req, res) => {res.sendFile(__dirname + '/pages/index.html');});
+app443.get('/', (req, res) => {res.sendFile(__dirname + '/pages/index.html');});
+
+app.listen(80, () => {console.log('80 port server start');});
+httpsServer.listen(443, () => {console.log('443 port server start');});
+[출처] 443 포트로 서버 배포 (SSL/TLS)|작성자 김지훈
